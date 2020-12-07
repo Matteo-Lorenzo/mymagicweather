b1c73a71be8931a8352b179e9d41557a5e6e7ddc
Matteo Lorenzo Bramucci <s1092174@studenti.univpm.it>
2020-12-07 18:09:55 +0100

    Primo caricamento progetto

---
+++ MyMagicWeather/src/main/java/it/abmlb/mmw/MyMagicWeatherApplication.java
@@ -0,0 +1,13 @@
+package it.abmlb.mmw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMagicWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMagicWeatherApplication.class, args);
+	}
+
+}
