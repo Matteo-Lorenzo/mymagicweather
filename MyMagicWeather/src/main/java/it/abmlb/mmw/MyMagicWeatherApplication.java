6dda96578701aac3db8bcf1e4c684974b493a561
Matteo Lorenzo Bramucci <s1092174@studenti.univpm.it>
2020-12-21 21:47:43 +0100

    Aggiornamento classe di bootstrap e aggiunta un'eccezione personalizzata

--- MyMagicWeather/src/main/java/it/abmlb/mmw/MyMagicWeatherApplication.java
+++ MyMagicWeather/src/main/java/it/abmlb/mmw/MyMagicWeatherApplication.java
@@ -1,8 +1,14 @@
 package it.abmlb.mmw;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import it.abmlb.mmw.services.StaticConfig;
 
 /**
  * @author matteolorenzo & agnese
@@ -14,9 +20,19 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class MyMagicWeatherApplication {
+	
+	@Autowired
+	private Environment env;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyMagicWeatherApplication.class, args);
+	}
+	
+	@PostConstruct
+	public void init() {
+		StaticConfig.setApikey(env.getProperty("mmw.apikey"));
+		StaticConfig.setOffset(Long.parseLong(env.getProperty("mmw.offset")));
+		StaticConfig.setCallOpenWeather(Boolean.parseBoolean(env.getProperty("mmw.callopenweather")));
 	}
 
 }
---
+++ MyMagicWeather/src/main/java/it/abmlb/mmw/exceptions/MmwInterpolationOutOfBoundsException.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package it.abmlb.mmw.exceptions;
+
+/**
+ * @author matteolorenzo & agnese
+ *
+ */
+public class MmwInterpolationOutOfBoundsException extends Exception {
+
+	
+	private static final long serialVersionUID = 2L;
+
+	
+	public MmwInterpolationOutOfBoundsException() {
+		super();
+	}
+
+	/**
+	 * @param message Description of the Exception
+	 */
+	public MmwInterpolationOutOfBoundsException(String message) {
+		super(message);
+	}
+	
+	
+
+}
